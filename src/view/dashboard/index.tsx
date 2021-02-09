07e8b18f8b716112d768cf4002f0becbdb39a558
krank <emosick@qq.com>
2021-02-09 18:45:27 +0800

    添加dashboard

---
+++ src/view/dashboard/index.tsx
@@ -0,0 +1,9 @@
+import * as React from 'react'
+
+// 主要展示区域引入的组件
+class Dashboard extends React.Component{
+    public render(){
+        return(<div>我是Dashboard</div>)
+    }
+}
+export default Dashboard
